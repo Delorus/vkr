b0e414a9baa6b6f079bc1bbdc6f9c2e3780623eb
mshherbakov <sherbakov.maks@gmail.com>
2019-02-10 14:52:40 +0500

    Добавлены и отформатированы листинги кодов

---
+++ src/reconn_util_test.go
@@ -0,0 +1,131 @@
+package gateway
+
+import (
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConnControl_NotifyClosed(t *testing.T) {
+	control := NewConnControl()
+
+	wg := new(sync.WaitGroup)
+	wg.Add(10)
+	for i := 0; i < 10; i++ {
+		go func() {
+			assert.NoError(t, control.WaitClosed())
+			wg.Done()
+		}()
+	}
+
+	control.NotifyClosed()
+
+	success := make(chan struct{})
+	go func() {
+		wg.Wait()
+		success <- struct{}{}
+	}()
+
+	select {
+	case <-success:
+	case <-time.After(100 * time.Millisecond):
+		t.Fatal("Time out")
+	}
+}
+
+func TestConnControl_Close(t *testing.T) {
+	control := NewConnControl()
+
+	success := make(chan struct{})
+	go func() {
+		if err := control.WaitClosed(); err != nil {
+			if err := control.WaitClosed(); err != nil {
+				success <- struct{}{}
+				return
+			}
+		}
+		t.Error("Should not have received a signal")
+	}()
+
+	control.Close()
+
+	select {
+	case <-success:
+	case <-time.After(100 * time.Millisecond):
+		t.Fatal("Time out")
+	}
+}
+
+func TestConnControl_IfConnAlreadyClosedNotBlocking(t *testing.T) {
+	control := NewConnControl()
+	control.NotifyClosed()
+
+	success := make(chan struct{})
+	go func() {
+		assert.NoError(t, control.WaitClosed())
+		success <- struct{}{}
+	}()
+
+	select {
+	case <-success:
+	case <-time.After(100 * time.Millisecond):
+		t.Fatal("Time out")
+	}
+}
+
+func TestConnControl_NotifyReopened(t *testing.T) {
+	control := NewConnControl()
+
+	wg := new(sync.WaitGroup)
+	wg.Add(10)
+	for i := 0; i < 10; i++ {
+		go func() {
+			assert.NoError(t, control.WaitReopened())
+			wg.Done()
+		}()
+	}
+
+	control.NotifyReopened()
+
+	success := make(chan struct{})
+	go func() {
+		wg.Wait()
+		success <- struct{}{}
+	}()
+
+	select {
+	case <-success:
+	case <-time.After(100 * time.Millisecond):
+		t.Fatal("Time out")
+	}
+}
+
+func TestConnControl_NotifyFailReopened(t *testing.T) {
+	control := NewConnControl()
+	control.status = CLOSE
+
+	wg := new(sync.WaitGroup)
+	wg.Add(10)
+	for i := 0; i < 10; i++ {
+		go func() {
+			assert.Error(t, control.WaitReopened())
+			wg.Done()
+		}()
+	}
+
+	control.NotifyFailReopened()
+
+	success := make(chan struct{})
+	go func() {
+		wg.Wait()
+		success <- struct{}{}
+	}()
+
+	select {
+	case <-success:
+	case <-time.After(100 * time.Millisecond):
+		t.Fatal("Time out")
+	}
+}
